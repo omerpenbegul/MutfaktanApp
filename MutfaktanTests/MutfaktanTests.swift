189e723b06da4b653b61479e2ff1ee835913a2d4
Ömer Penbegül <omrpnbgl@gmail.com>
2025-05-17 00:22:23 +0300

    Initial Commit

---
+++ MutfaktanTests/MutfaktanTests.swift
@@ -0,0 +1,17 @@
+//
+//  MutfaktanTests.swift
+//  MutfaktanTests
+//
+//  Created by Ömer  PENBEGÜL on 17.05.2025.
+//
+
+import Testing
+@testable import Mutfaktan
+
+struct MutfaktanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
